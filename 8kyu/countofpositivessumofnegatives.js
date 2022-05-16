51385197d1a692393785f2c8df8f597adf9360df
Ingmar Soares <102437949+IngmarSoares@users.noreply.github.com>
2022-05-16 23:27:27 +0100

    Update countofpositivessumofnegatives.js
    iPad midnight commander formatted text in a wrong way.

--- 8kyu/countofpositivessumofnegatives.js
+++ 8kyu/countofpositivessumofnegatives.js
@@ -1,17 +1,12 @@
 function countPositivesSumNegatives(input) {
-    if (input == null || input.length == 0)
-          return [];
-              
-                  var positive = 0;
-                      var negative = 0;
-                          
-                              for (var i=0, l=input.length; i<l; ++i)
-                                  {
-                                        if (input[i] > 0)
-                                                ++ positive;
-                                                      else
-                                                              negative += input[i];
-                                                                  }
-                                                                      
-                                                                          return [positive, negative];
-                                                                          }
+  let result = [0,0]
+  if (input === null || input.length < 1) {return []}
+  for (i=0 ; i < input.length ; i++) {
+    if (0 - input[i] > 0) {
+      result[1] += input[i]
+    } else if (0 - input[i] < 0) {
+      result[0] += 1
+    }
+  }
+  return result
+}
