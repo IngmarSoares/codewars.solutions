e2dac370cef4af99b36800e72f821f2a97ab63ec
Ingmar Soares <102437949+IngmarSoares@users.noreply.github.com>
2022-11-04 11:44:04 +0100

    Update numbersdissapear.js

--- 7kyu/numbersdissapear.js
+++ 7kyu/numbersdissapear.js
@@ -1,1 +1,16 @@
-Thu Jun 09 2022 10:41:43 GMT+0200 (Central European Summer Time)
+var findDisappearedNumbers = function (nums) {
+    var numLength = nums.length;
+    nums.sort(function (a, b) { return a - b });
+    for (var i = 0; i < nums.length - 1; i++) {
+        if (nums[i + 1] === nums[i]) {
+            nums.splice(i, 1);
+        }
+    }
+    for (var k = 0; k < nums.length; k++) {
+        for (var j = 1; j <= numLength; j++) {
+            if (nums[k] !== j) {
+                return j;
+            }
+        }
+    }
+};
