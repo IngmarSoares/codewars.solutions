272618e7f524fde607f0c6bf86702ab26fee8064
Ingmar Soares <102437949+IngmarSoares@users.noreply.github.com>
2022-11-04 11:45:34 +0100

    Update findthemissingcharacter.js

--- 7kyu/findthemissingcharacter.js
+++ 7kyu/findthemissingcharacter.js
@@ -1,1 +1,16 @@
-Fri Oct 07 2022 11:36:37 GMT+0200 (Central European Summer Time)
+const findMissingLetter = (array) => {
+  // we can skip the 1st letter
+  for (let i = 1; i < array.length; i++) {
+    // get the char code of the previous letter
+    const prev = array[i - 1].charCodeAt();
+    // get the char code of the current letter
+    const current = array[i].charCodeAt();
+    
+    if (current - prev !== 1) { // if the difference between current and previous is not 1
+      // get the character after the previous 
+      return String.fromCharCode(prev + 1);
+    }
+  }
+  
+  return null; // if nothing is found
+}
